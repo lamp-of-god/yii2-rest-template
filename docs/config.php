843f3f79f59fdd9f2de6469551e4c0c308d98b86
sergey_rus@bk.ru <Cru21Sader>
2020-04-16 21:52:36 +0500

    Fix

--- docs/config.php
+++ docs/config.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'id' => 'docs',
-    'basePath' => dirname(__DIR__),
+    'basePath' => __DIR__,
     'controllerNamespace' => 'docs\controllers',
     'modules' => [
         'v1' => ['class' => 'docs\modules\v1\Module'],
